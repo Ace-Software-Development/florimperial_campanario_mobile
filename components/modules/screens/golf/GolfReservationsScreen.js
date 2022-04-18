d42cb1356a366211d31c325df35d0f500a82cfac
DanielCruzAr <daniel_cruzar@outlook.com>
2022-04-18 15:20:30 -0500

    Reservaciones

--- components/modules/screens/golf/GolfReservationsScreen.js
+++ components/modules/screens/golf/GolfReservationsScreen.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Switch, StyleSheet } from 'react-native';
-import { Title, ScreenContainer, P, Subtitle, ActionBtn } from '../../../ui/CampanarioComponents';
+import { Title, ScreenContainer, P, Subtitle, ActionBtn, Btn } from '../../../ui/CampanarioComponents';
 import DateOption from '../../../ui/DateOption';
 import CapsuleBtn from '../../../ui/CapsuleBtn';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export default function GolfReservationsScreen(props) {
+	const [date, setDate] = useState();
+	const [hour, setHour] = useState();
+
+	const printDate = () => {
+		var reservation={"date_time" : date, "hour" : hour};
+		var JSONreservation=JSON.stringify(reservation);
+		console.log(date);
+		console.log(hour);
+		console.log(JSONreservation.date_time, JSONreservation.hour);
+	}
+
 	return (
 		<ScreenContainer>
 			
@@ -36,21 +48,30 @@ export default function GolfReservationsScreen(props) {
 
 				{/* Date picker */}
 				<View style={style.datePickerContainer}>
-					<DateOption defaultActive={true} datetime={'2022-04-20'} date='lun' day='20' onClick={() => {}} />
-					<DateOption defaultActive={false} datetime={'2022-04-20'} date='mar' day='21' onClick={() => {}} />
-					<DateOption defaultActive={false} datetime={'2022-04-22'} date='mie' day='22' onClick={() => {}} />
+					<DateOption defaultActive={false} datetime={'2022-04-20'} date='lun' day='20' value={date} onClick={() => setDate('2022-04-20')} />
+					<DateOption defaultActive={false} datetime={'2022-04-20'} date='mar' day='21' value={date} onClick={() => setDate('2022-04-20')} />
+					<DateOption defaultActive={false} datetime={'2022-04-22'} date='mie' day='22' value={date} onClick={() => setDate('2022-04-22')} />
 				</View>
 
 				{/* Hour picker */}
 				<View style={style.timePickerContainer} >
+					<TouchableOpacity value={hour} onPress={() => setHour('12:00')}>
 					<CapsuleBtn title='12:00 am' subtitle='HOYO 1' />
+					</TouchableOpacity>
+					{/*
 					<CapsuleBtn title='1:00 pm' subtitle='HOYO 1' />
 					<CapsuleBtn title='2:00 pm' subtitle='HOYO 1' />
 					<CapsuleBtn title='3:00 pm' subtitle='HOYO 1' />
 					<CapsuleBtn title='4:00 pm' subtitle='HOYO 1' />
 					<CapsuleBtn title='5:30 pm' subtitle='HOYO 1' />
 					<CapsuleBtn title='10:30 pm' subtitle='HOYO 1' />
+					*/}
 				</View>
+
+				<Btn
+					onPress={() => printDate()}
+				>
+				</Btn>
 
 			</View>
 
