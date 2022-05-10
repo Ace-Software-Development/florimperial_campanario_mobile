e0b15b1ecd8ee576791d9f4432616bc66264e88e
Joaquinzer <joaquin@DESKTOP-0MAU4T7.localdomain>
2022-05-10 12:54:44 -0500

    Borrar anuncios despues de fecha de vencimiento

--- components/modules/screens/newsletter/NewsletterHomeScreen.js
+++ components/modules/screens/newsletter/NewsletterHomeScreen.js
@@ -15,6 +15,7 @@ export default function NewsletterHomeScreen(props) {
 		const fetchImages = async () => {
 			let query = new Parse.Query('Anuncio');
 			query.equalTo('eliminado', false);
+			query.greaterThan('fechaVencimiento', new Date());
 			const results = await query.find();
 			setPosters(results);
 		};
